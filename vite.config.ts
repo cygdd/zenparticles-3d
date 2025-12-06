fd1c6bb108e3444c1924d8ae1edc6564557a8fd4
zero <122187811+cygdd@users.noreply.github.com>
2025-12-06 21:08:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: "/zenparticles-3d/",  // <--- 加上这一行！注意前后都要有斜杠
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
